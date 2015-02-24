d1db890915f75f1d604f5d1112cd73ae7dce902b
ldocao <ldocao@gmail.com>
2015-02-24 10:29:11 +0900

    add 2 functions. tested with identity matrix

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,30 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+    inv <- NULL #flag to check if inverse has been computed
+    set <- function(y){
+        x <<- y
+        inverse <- NULL
+    }
 
+    get <- function() x #get the original matrix
+    setinverse <- function(inverse) inv <<- inverse
+    getinverse <- function() inv
+    list(set=set,get=get,setinverse=setinverse,getinverse=getinverse)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinverse()
+    if(!is.null(inv)){
+        message("getting cached data")
+        return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data,...)
+    x$setinverse(inv)
+    inv
 }
